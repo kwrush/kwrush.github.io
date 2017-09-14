a4e006fea799743934be36cd1b272d16290e770e
kwrush <kaiwang.me@gmail.com>
2017-09-15 00:17:04 +0200

    create the basic avatar
    a simple avatar
    
    update
    
    update

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const colors = {
+    skin: 0xeec1ad,
+    hair: 0x2c222b,
+    nose: 0xeac086,
+    lip: 0xe35d6a,
+    eye: 0xfcfcfc,
+    iris: 0x090806,
+};
---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const webpackCommon = require('./webpack.common.js');
+
+module.exports = merge(webpackCommon, {
+    plugins: [
+        new UglifyJSPlugin({
+            sourceMap: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
+})
