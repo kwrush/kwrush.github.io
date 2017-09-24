69388e4f38af578a72196be06632272746eaace6
Kai Wang <k.wang0905@hotmail.com>
2017-09-24 22:21:25 +0200

    separate scene creation

--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,10 @@ export const colors = {
     lip: 0xe35d6a,
     eye: 0xfcfcfc,
     iris: 0x090806,
+    glasses: 0x5dade2
 };
+
+export const names = {
+    glasses: '[THREE Glasses]',
+    avatar: '[THREE Avatar]'
+}
