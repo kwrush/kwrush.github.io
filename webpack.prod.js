77fab1f1c5b572af7f022ed3d32f822ceb535adb
Kai Wang <k.wang0905@hotmail.com>
2017-10-24 22:45:17 +0200

    reduce bundle size

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,23 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const WebpackCdnPlugin = require('webpack-cdn-plugin');
 const autoprefixer = require('autoprefixer');
 const webpackCommon = require('./webpack.common.js');
 
 module.exports = merge(webpackCommon, {
 	devtool: 'source-map',
     plugins: [
+        new WebpackCdnPlugin({
+            modules: [
+                {
+                    name: 'three',
+                    var: 'THREE',
+                    path: 'build/three.min.js'
+                }
+            ],
+            publicPath: '/node_modules'
+        }),
         new UglifyJSPlugin({
             sourceMap: true
         }),
