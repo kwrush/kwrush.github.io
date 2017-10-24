7cf99cad0f92ecbe59371edd506d47005496668e
kwrush <kaiwang.me@gmail.com>
2017-10-24 22:45:17 +0200

    reduce bundle size

--- webpack.common.js
+++ webpack.common.js
@@ -38,6 +38,6 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new HtmlWebpackPlugin({ template: './src/index.html' })
     ]
 }
