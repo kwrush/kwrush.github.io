cc57a52aaa2c802f0c1b62fd0e0b3d05a4686543
kwrush <kaiwang.me@gmail.com>
2020-10-26 19:22:03 +0100

    Build

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// TODO: Rewrite webpack config
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
